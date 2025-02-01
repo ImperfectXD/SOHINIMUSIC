bf33c3e7f223b73fcc1addbda0ba011db3081dd1
Gᴏᴅғᴀᴛʜᴇʀ <104144725+mrtosumemon@users.noreply.github.com>
2025-02-01 11:44:05 +0530

    Update info.py

--- SACHIN_MUSIC/plugins/mics/info.py
+++ SACHIN_MUSIC/plugins/mics/info.py
@@ -11,7 +11,7 @@ from typing import Union, Optional
 
 EVAA = [
     [
-        InlineKeyboardButton(text="ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ", url=f"https://t.me/SUCHI_MUSIC_BOT?startgroup=true"),
+        InlineKeyboardButton(text="ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ", url=f"https://t.me/DollxMusic_bot?startgroup=true"),
     ],
 ]
 
