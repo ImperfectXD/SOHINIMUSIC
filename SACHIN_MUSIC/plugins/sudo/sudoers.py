da6d25ad8a498f9071c7e42ab695b416f301df80
𓆩 𝐒 𝐇 𝐔 𝐁 𝐇 𝐎 𓆪 <shubhendugarai917@gmail.com>
2025-02-18 01:29:41 +0530

    Update sudoers.py

--- SACHIN_MUSIC/plugins/sudo/sudoers.py
+++ SACHIN_MUSIC/plugins/sudo/sudoers.py
@@ -61,7 +61,7 @@ async def sudoers_list(client, message: Message):
 async def check_sudo_list(client, callback_query: CallbackQuery):
     keyboard = []
     if callback_query.from_user.id not in SUDOERS:
-        return await callback_query.answer("𝐊𝐘𝐀 𝐑𝐄 𝐑𝐀𝐍𝐃𝐈 𝐊𝐄 𝐒𝐔𝐃𝐎 𝐋𝐈𝐒𝐓 𝐃𝐄𝐊𝐇𝐄𝐆𝐀 𝐓𝐄𝐑𝐈 𝐌𝐀 𝐊𝐈 𝐂𝐇𝐔𝐓 𝐉𝐇𝐀𝐓 𝐊𝐄 𝐁𝐀𝐀𝐋 [ @LEGEND_MICKEY ]", show_alert=True)
+        return await callback_query.answer("𝐊𝐘𝐀 𝐑𝐄 𝐑𝐀𝐍𝐃𝐈 𝐊𝐄 𝐒𝐔𝐃𝐎 𝐋𝐈𝐒𝐓 𝐃𝐄𝐊𝐇𝐄𝐆𝐀 𝐓𝐄𝐑𝐈 𝐌𝐀 𝐊𝐈 𝐂𝐇𝐔𝐓 𝐉𝐇𝐀𝐓 𝐊𝐄 𝐁𝐀𝐀𝐋 [ @hey_cuties ]", show_alert=True)
     else:
         user = await app.get_users(OWNER_ID)
 
