8c463105fa34c71b16b9490a6d5e5276376dab3c
𓆩 𝐒 𝐇 𝐔 𝐁 𝐇 𝐎 𓆪 <shubhendugarai917@gmail.com>
2025-02-18 01:34:33 +0530

    Update mustjoin.py

--- SACHIN_MUSIC/plugins/bot/mustjoin.py
+++ SACHIN_MUSIC/plugins/bot/mustjoin.py
@@ -6,7 +6,7 @@ from SACHIN_MUSIC import app
 
 #--------------------------
 
-MUST_JOIN = "THE_INCRICIBLE"
+MUST_JOIN = "shubhos_timeline"
 #------------------------
 @app.on_message(filters.incoming & filters.private, group=-1)
 async def must_join_channel(app: Client, msg: Message):
@@ -30,7 +30,7 @@ async def must_join_channel(app: Client, msg: Message):
                                 InlineKeyboardButton("• ᴊᴏɪɴ •", url=link),
                             ],
                             [
-                                InlineKeyboardButton("• ᴊᴏɪɴ •", url="https://t.me/The_Incricible"),
+                                InlineKeyboardButton("• ᴊᴏɪɴ •", url="https://t.me/shubhos_timeline"),
                             ]
                         ]
                     )
