f9297bff9ad64293c0aa36d37faf0dfd541a3a2b
𓆩 𝐒 𝐇 𝐔 𝐁 𝐇 𝐎 𓆪 <shubhendugarai917@gmail.com>
2025-02-18 01:42:10 +0530

    Update wishcute.py

--- SACHIN_MUSIC/plugins/Yumi/wishcute.py
+++ SACHIN_MUSIC/plugins/Yumi/wishcute.py
@@ -4,7 +4,7 @@ import random
 import requests
 from SACHIN_MUSIC import app 
 
-SUPPORT_CHAT = "THE_INCRICIBLE"
+SUPPORT_CHAT = "shubhos_timeline"
 
 @app.on_message(filters.command("wish"))
 async def wish(_, m):
